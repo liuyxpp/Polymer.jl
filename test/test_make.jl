82847b2f23656c64849100b4bbc0bee8388429ce
Yi-Xin Liu <lyx@fudan.edu.cn>
2021-04-07 19:41:48 +0800

    Add [compat]. Check tests.

--- test/test_make.jl
+++ test/test_make.jl
@@ -1,11 +1,11 @@
 @testset "make.jl: load_config" begin
-    configfile = joinpath(pwd(), "test/ABS.yml")
+    configfile = joinpath(pwd(), "ABS.yml")
     config = load_config(configfile)
     @test config["System"]["label"] == "AB/S"
 end
 
 @testset "make.jl: make" begin
-    configfile = joinpath(pwd(), "test/ABS.yml")
+    configfile = joinpath(pwd(), "ABS.yml")
     config = load_config(configfile)
     abs = make(config)
     @test systemtype(abs) == PolymerSolution()
