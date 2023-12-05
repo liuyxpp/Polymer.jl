8c7807e189db3a35cd328140560b64ef18a3981f
DShKM118 <zqzycjqw@sina.com>
2023-12-05 17:06:27 +0800

    add a keyword argument 'argcheck=true' in the inner constructor of srtuct BlockCopolymer

--- src/types.jl
+++ src/types.jl
@@ -125,8 +125,8 @@ struct BlockCopolymer{T<:AbstractBlock} <: AbstractPolymer
     label::Symbol
     blocks::Vector{T}
 
-    function BlockCopolymer(label::Symbol, blocks::Vector{T}) where {T<:PolymerBlock}
-        @argcheck _isachain(blocks)
+    function BlockCopolymer(label::Symbol, blocks::Vector{T}; argcheck=true) where {T<:PolymerBlock}
+        argcheck && @argcheck _isachain(blocks)
         new{T}(label, blocks)
     end
 end
